b4f5474fdac1894e607d47fc7650ab87569052a0
Paulo Henrique <paulo.13.henrique800@gmail.com>
2024-09-08 19:18:22 -0300

    criando camada de serviços para a entidade livro

---
+++ com.api_rest_livros/src/main/java/com/projeto_pessoal/com/api_rest_livros/services/LivroService.java
@@ -0,0 +1,47 @@
+package com.projeto_pessoal.com.api_rest_livros.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto_pessoal.com.api_rest_livros.entities.Livro;
+import com.projeto_pessoal.com.api_rest_livros.repositories.LivroRepository;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repositorio;
+	
+	
+	public List<Livro>findAll(){
+		List<Livro>livros= repositorio.findAll();
+		
+		return livros;
+	}
+	public Livro findById(Integer id) {
+		Livro livro= repositorio.findById(id).get();
+		
+		return livro;
+	}
+	public void deleteById(Integer id) {
+		repositorio.deleteById(id);
+	}
+	public Livro update(int id, Livro novoLivro) {
+		Livro livro= repositorio.getReferenceById(id);
+		updateData(livro,novoLivro);
+		return repositorio.save(livro);
+	}
+	private void updateData(Livro livro, Livro novoLivro) {
+		livro.setTitulo(novoLivro.getTitulo());
+		livro.setSumario(novoLivro.getSumario());
+		livro.setIsbn(novoLivro.getIsbn());
+		livro.setGenre(novoLivro.getGenre());
+		livro.setDataDePublicacao(novoLivro.getDataDePublicacao());
+		
+	}
+	public Livro insert(Livro livro) {
+		return repositorio.save(livro);
+	}
+}
