6f66c055be6a53d1ad78bd461b6eff93a45c3149
Paulo Henrique <paulo.13.henrique800@gmail.com>
2024-09-08 17:04:11 -0300

    projeto criado

---
+++ com.api_rest_livros/src/main/java/com/projeto_pessoal/com/api_rest_livros/Application.java
@@ -0,0 +1,13 @@
+package com.projeto_pessoal.com.api_rest_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ com.api_rest_livros/src/test/java/com/projeto_pessoal/com/api_rest_livros/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_pessoal.com.api_rest_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
